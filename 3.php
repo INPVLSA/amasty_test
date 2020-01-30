b82225c61aba1bd49aaa3ee2b2b58bbc58805584
Vladislav Marudenko <shockvladislav@gmail.com>
2020-01-30 17:15:33 +0300

    Done.

--- 3.php
+++ 3.php
@@ -1,6 +1,10 @@
 <?php
+/**
+ * @var string $argv
+ */
+
 // Checking all elements, setting int type to numeric
-$array = array_map('intval', array_filter($argv, 'is_numeric'));
+$array = array_map('intval', array_filter(explode(' ', $argv[1]), 'is_numeric'));
 
 // Drop duplicates
 $array = array_unique($array);
